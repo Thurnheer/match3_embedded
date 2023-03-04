531bef6eeb1a06dd683b5562ba3226204ec000a5
Christoph Thurnheer <christoph.thurnheer@thisag.ch>
2023-03-04 19:27:50 +0100

    now works with cmake and on hw

---
+++ match3_stm/TouchGFX/build/STM32H735G-DK/TouchGFX/generated/images/src/image_img3.d
@@ -0,0 +1,5 @@
+TouchGFX/build/STM32H735G-DK/TouchGFX/generated/images/src/image_img3.o: \
+ TouchGFX/generated/images/src/image_img3.cpp \
+ Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
+
+Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
